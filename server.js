2d816be52ba31378307112778781a401af8df28a
BayanBanat <bayankbanat99@gmail.com>
2023-03-23 19:10:53 +0000

    lab13

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 'use strict'
 const express = require('express');
-const cors= require('cors');
+// const cors= require('cors');
 const axios=require('axios');
+const bodyParser = require('body-parser');
+const { Client } = require('pg');
 require('dotenv').config();
+const url=process.env.URL;
+const client = new Client(url);
 const app = express();
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(cors());
 const PORT=process.env.PORT;
 const apiKey=process.env.API_KEY;
 
-
-app.get('/', homePageHandler);  //rout 1
-app.get('/favorite', favoriteHandler);  //rout 2
+// Routs:
+app.get('/', homePageHandler);  
+app.get('/favorite', favoriteHandler);  
 app.get('/example',errorHandler2)
-app.get('/trending',trendingHandler) //rout 4
-app.get('/search',searchMoviesHandler) //rout 5
-app.get('/populerMovies', populerHandler) //rout 6
-app.get('/searchMovies',planeSearchMoviesHandler) //rout 7
-app.get('*', errorHandler);   //rout 3
+app.get('/trending',trendingHandler) 
+app.get('/search',searchMoviesHandler)
+app.get('/populerMovies', populerHandler)
+app.get('/searchMovies',planeSearchMoviesHandler)
+app.post('/addMoveis',addMoviesHandger)
+app.get('/getAllMovies',getAllMovies)
+app.get('*', errorHandler);   
 
 //function
 function homePageHandler(req, res) {
@@ -24,7 +32,7 @@ function homePageHandler(req, res) {
     let newMovie = new Movie(moviesData.title, moviesData.poster_path, moviesData.overview);
     result.push(newMovie);
     res.json(result);
-    // console.log("bayan");
+    console.log("bayan");
 
 }
 function Movie(title, posterPath, overview) {
@@ -43,14 +51,10 @@ function errorHandler(req, res) {
         res.status(404).send("Not Found")     
 }
 
-function errorHandler2(req,res){
-    axios.get('https://example.com')
-    .then((result)=> {
-    res.json(result);
-    })
-    .catch((error)=> {
-        res.status(500).send("Sorry, something went wrong")
-    });
+
+ 
+function errorHandler2(err,req,res){
+    res.status(500).send(err)
 }
 
 
@@ -66,8 +70,8 @@ function errorHandler2(req,res){
 
         res.json(dataMovie);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch((error)=>{
+        errorHandler2(error,req,res);
 
     })
 
@@ -86,8 +90,8 @@ function errorHandler2(req,res){
 
         res.json(dataMovie);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch((error)=>{
+        errorHandler2(error,req,res);
 
     })
 
@@ -114,8 +118,8 @@ function errorHandler2(req,res){
         })
         res.json(searchMovieData);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch((error)=>{
+        errorHandler2(error,req,res);
     })
  }
  function planeSearchMoviesHandler(req,res){
@@ -129,8 +133,8 @@ function errorHandler2(req,res){
         })
         res.json(searchMovieData);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch((error)=>{
+        errorHandler2(error,req,res);
     })
  }
 
@@ -143,9 +147,40 @@ function errorHandler2(req,res){
     this.overview=overview;
 
  }
+ function addMoviesHandger(req,res){
+    let {title,poster_path,overview} =req.body; //destructuring
+    let sql=`INSERT INTO movies (title ,poster_path,overview)
+    VALUES ($1,$2,$3) RETURNING * `
+    let values=[title,poster_path,overview]
+    client.query(sql,values).then((result)=>{
+        console.log(req.body);
+        res.status(201).json(result.rows)
+    })
+    .catch((error)=>{
+        errorHandler2(error,req,res);
+    })
+    
+    
+ }
+ function getAllMovies(req,res){
+    let sql=`SELECT * FROM movies`;
+    client.query(sql).then((result)=>{
+        res.json(result.rows)
+    })
+    .catch((error)=>{
+        errorHandler2(error,req,res);
+    })
+ }
 
 
+ client.connect().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`listening on port${PORT}`);
+    })
 
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+}).catch((error)=>{
+    errorHandler2(error,req,res);
 })
+
+
+
